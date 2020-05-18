6df440d0b9d724ef52f2ff530346bf302e797569
Nellie <fabybacenelson@gmail.com>
2020-05-18 13:39:54 -0700

    fixed tests

--- src/__tests__/pqbd.test.ts
+++ src/__tests__/pqbd.test.ts
@@ -1,7 +1,16 @@
 import '../index';
 
-let testArr = [1, 2, 3, 4];
-
 test('qoq', () => {
-  expect(testArr.qoq()).toStrictEqual([2, 3, 4]);
+  let testArr = [1, 2, 3, 4];
+  expect(testArr.qoq()).toStrictEqual(1);
+});
+
+test('bob', () => {
+  let testArr = [1, 2, 3, 4];
+  expect(testArr.bob(5)).toStrictEqual(5);
+});
+
+test('dod', () => {
+  let testArr = [1, 2, 3, 4];
+  expect(testArr.dod(0)).toStrictEqual(5);
 });
